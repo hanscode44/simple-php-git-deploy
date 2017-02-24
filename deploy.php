7044fa7057563c443a7083c316dfd6046cf202ee
Hans Loos <hansloos.g1@gmail.com>
2017-02-24 15:52:24 +0100

    make configfile mandatory

--- deploy.php
+++ deploy.php
@@ -4,209 +4,41 @@
  *
  * Automatically deploy the code using PHP and Git.
  *
- * @version 1.3.1
- * @link    https://github.com/markomarkovic/simple-php-git-deploy/
+ * @version 2.0
+ * @link    https://github.com/hanscode44/simple-php-git-deploy
  */
 
-// =========================================[ Configuration start ]===
+// =========================================[ Configuration check ]===
 
 /**
- * It's preferable to configure the script using `deploy-config.php` file.
+ * It's mandatory to configure the script using `deploy-config.php` file.
  *
  * Rename `deploy-config.example.php` to `deploy-config.php` and edit the
- * configuration options there instead of here. That way, you won't have to edit
- * the configuration again if you download the new version of `deploy.php`.
+ * configuration options. That way, you won't have to edit the configuration
+ * again if you download the new version of `deploy.php`.
  */
 if (file_exists(basename(__FILE__, '.php') . '-config.php')) {
     define('CONFIG_FILE', basename(__FILE__, '.php') . '-config.php');
     require_once CONFIG_FILE;
 } else {
-    define('CONFIG_FILE', __FILE__);
+    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found', true, 404);
+    die("<h2>No config<br>Add a config file to work with!</h2>");
 }
 
-/**
- * Protect the script from unauthorized access by using a secret access token.
- * If it's not present in the access URL as a GET variable named `sat`
- * e.g. deploy.php?sat=Bett...s the script is not going to deploy.
- *
- * @var string
- */
-if (!defined('SECRET_ACCESS_TOKEN')) {
-    define('SECRET_ACCESS_TOKEN', 'BetterChangeMeNowOrSufferTheConsequences');
-}
-
-/**
- * The address of the remote Git repository that contains the code that's being
- * deployed.
- * If the repository is private, you'll need to use the SSH address.
- *
- * @var string
- */
-if (!defined('REMOTE_REPOSITORY')) {
-    define('REMOTE_REPOSITORY', 'https://github.com/markomarkovic/simple-php-git-deploy.git');
-}
-
-/**
- * The branch that's being deployed.
- * Must be present in the remote repository.
- *
- * @var string
- */
-if (!defined('BRANCH')) {
-    define('BRANCH', 'master');
-}
-
-/**
- * The location that the code is going to be deployed to.
- * Don't forget the trailing slash!
- *
- * @var string Full path including the trailing slash
- */
-if (!defined('TARGET_DIR')) {
-    define('TARGET_DIR', '/tmp/simple-php-git-deploy/');
-}
-
-/**
- * Whether to delete the files that are not in the repository but are on the
- * local (server) machine.
- *
- * !!! WARNING !!! This can lead to a serious loss of data if you're not
- * careful. All files that are not in the repository are going to be deleted,
- * except the ones defined in EXCLUDE section.
- * BE CAREFUL!
- *
- * @var boolean
- */
-if (!defined('DELETE_FILES')) {
-    define('DELETE_FILES', false);
-}
-
-/**
- * The directories and files that are to be excluded when updating the code.
- * Normally, these are the directories containing files that are not part of
- * code base, for example user uploads or server-specific configuration files.
- * Use rsync exclude pattern syntax for each element.
- *
- * @var serialized array of strings
- */
-if (!defined('EXCLUDE')) {
-    define(
-        'EXCLUDE',
-        serialize(
-            array(
-                '.git',
-            )
-        )
-    );
-}
-
-/**
- * Temporary directory we'll use to stage the code before the update. If it
- * already exists, script assumes that it contains an already cloned copy of the
- * repository with the correct remote origin and only fetches changes instead of
- * cloning the entire thing.
- *
- * @var string Full path including the trailing slash
- */
-if (!defined('TMP_DIR')) {
-    define('TMP_DIR', '/tmp/spgd-' . md5(REMOTE_REPOSITORY) . '/');
-}
-
-/**
- * Whether to remove the TMP_DIR after the deployment.
- * It's useful NOT to clean up in order to only fetch changes on the next
- * deployment.
- */
-if (!defined('CLEAN_UP')) {
-    define('CLEAN_UP', true);
-}
-
-/**
- * Output the version of the deployed code.
- *
- * @var string Full path to the file name
- */
-if (!defined('VERSION_FILE')) {
-    define('VERSION_FILE', TMP_DIR . 'VERSION');
-}
-
-/**
- * Time limit for each command.
- *
- * @var int Time in seconds
- */
-if (!defined('TIME_LIMIT')) {
-    define('TIME_LIMIT', 30);
-}
-
-/**
- * OPTIONAL
- * Backup the TARGET_DIR into BACKUP_DIR before deployment.
- *
- * @var string Full backup directory path e.g. `/tmp/`
- */
-if (!defined('BACKUP_DIR')) {
-    define('BACKUP_DIR', false);
-}
-
-/**
- * OPTIONAL
- * Whether to invoke composer after the repository is cloned or changes are
- * fetched. Composer needs to be available on the server machine, installed
- * globaly (as `composer`). See http://getcomposer.org/doc/00-intro.md#globally
- *
- * @var boolean Whether to use composer or not
- * @link http://getcomposer.org/
- */
-if (!defined('USE_COMPOSER')) {
-    define('USE_COMPOSER', false);
-}
-
-/**
- * OPTIONAL
- * The options that the composer is going to use.
- *
- * @var string Composer options
- * @link http://getcomposer.org/doc/03-cli.md#install
- */
-if (!defined('COMPOSER_OPTIONS')) {
-    define('COMPOSER_OPTIONS', '--no-dev');
-}
-
-/**
- * OPTIONAL
- * The COMPOSER_HOME environment variable is needed only if the script is
- * executed by a system user that has no HOME defined, e.g. `www-data`.
- *
- * @var string Path to the COMPOSER_HOME e.g. `/tmp/composer`
- * @link https://getcomposer.org/doc/03-cli.md#composer-home
- */
-if (!defined('COMPOSER_HOME')) {
-    define('COMPOSER_HOME', false);
-}
-
-/**
- * OPTIONAL
- * Email address to be notified on deployment failure.
- *
- * @var string A single email address, or comma separated list of email addresses
- *      e.g. 'someone@example.com' or 'someone@example.com, someone-else@example.com, ...'
- */
-if (!defined('EMAIL_ON_ERROR')) {
-    define('EMAIL_ON_ERROR', false);
-}
-
-// ===========================================[ Configuration end ]===
-
-// If there's authorization error, set the correct HTTP header.
-if (!isset($_GET['sat']) ||
-    $_GET['sat'] !== SECRET_ACCESS_TOKEN ||
-    SECRET_ACCESS_TOKEN === 'BetterChangeMeNowOrSufferTheConsequences'
-) {
+// check if the Secret Access Token is changed,
+if (!isset($_GET['sat']) || $_GET['sat'] !== SECRET_ACCESS_TOKEN) {
     header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden', true, 403);
+    die('<h2>ACCESS DENIED!</h2>');
+} else if (SECRET_ACCESS_TOKEN === 'BetterChangeMeNowOrSufferTheConsequences') {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden', true, 403);
+    die("<h2>You're suffering the consequences!<br>Change the SECRET_ACCESS_TOKEN from it's default value!</h2>");
 }
+// ==========================================[ Configuration check end ]===
+
 ob_start();
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -238,16 +70,9 @@ ob_start();
     </style>
 </head>
 <body>
-<?php
-if (!isset($_GET['sat']) || $_GET['sat'] !== SECRET_ACCESS_TOKEN) {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden', true, 403);
-    die('<h2>ACCESS DENIED!</h2>');
-}
-if (SECRET_ACCESS_TOKEN === 'BetterChangeMeNowOrSufferTheConsequences') {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden', true, 403);
-    die("<h2>You're suffering the consequences!<br>Change the SECRET_ACCESS_TOKEN from it's default value!</h2>");
-}
-?>
+
+
+
 <pre>
 
 Checking the environment ...
@@ -268,7 +93,7 @@ Running as <b><?php echo trim(shell_exec('whoami')); ?></b>.
         $requiredBinaries[] = 'composer --no-ansi';
     }
     foreach ($requiredBinaries as $command) {
-        $path = trim(shell_exec('which ' . $command));
+        $path = trim(shell_exec('export PATH=$PATH; which ' . $command));
         if ($path == '') {
             header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
             die(
